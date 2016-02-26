960e55e093d82a2b62ce73a5c184d9c86a429978
Pavel <cp@zakupi.net>
2016-02-26 16:45:56 +0300

    perviy

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=loaddata',
+    'username' => 'loaddata',
+    'password' => '123',
+    'charset' => 'utf8',
+];
